aad6ff2adb2b41e928ef7475a065a7774853b6ea
Alexander G. Yang <spark8402@rogers.com>
2019-06-29 10:45:17 -0400

    assignment done

--- calculator.js
+++ calculator.js
@@ -1,10 +1,14 @@
-function add() {
+function add(num1,num2) {
+    return num1 + num2
 }
 
-function subtract() {
+function subtract(num1,num2) {
+    return num1 - num2
 }
 
-function sum() {
+function sum(nums) {
+    return nums.reduce((a, b) => a + b, 0)
+
 }
 
 module.exports = {add, subtract, sum}
